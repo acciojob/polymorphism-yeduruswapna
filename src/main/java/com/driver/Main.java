7c71c1314812bf0bbffbffd733deec4d1effef39
yeduruswapna <swapnayeduru@gmail.com>
2023-05-24 22:16:09 +0530

    First Commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-
+    public static void main() {
+        Product p = new Product();
+        System.out.println(p.product(2, 3));
+        System.out.println(p.product(5,6,7));
+        System.out.println(p.product(4.5, 9.0));
+    }
 }
+
---
+++ src/main/java/com/driver/Product.java
@@ -0,0 +1,16 @@
+package com.driver;
+
+public class Product {
+
+     public int product(int x, int y){
+        return x*y;
+     }
+
+    public int product(int x, int y, int z){
+        return x*y*z;
+    }
+
+    public double product(double x, double y){
+        return (double)x*y;
+    }
+}
